55288f296d87e0839a8e331292144eca4f7f30f2
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-11-21 17:34:00 +0300

    implement error colors

--- main.py
+++ main.py
@@ -30,28 +30,48 @@ class App(ctk.CTk):
             self.map_widget.set_address(location.address)
             self.input_string.set("")
         else:
-            print("invalid")
+            self.map_widget.location_entry.error_animation()
 
 class MapWidget(tkintermapview.TkinterMapView):
     def __init__(self,parent,input_string,submit_location):
         super().__init__(parent)
         self.grid(row=0,column=1,sticky="nsew")
 
-        LocationEntry(self,input_string,submit_location)
+        self.location_entry = LocationEntry(self,input_string,submit_location)
         # self.set_tile_server(TERRAIN_URL)
 
 class LocationEntry(ctk.CTkEntry):
     def __init__(self,parent,input_string,submit_location):
+        self.color_index = 15
+        color = COLOR_RANGE[self.color_index]
+        self.error = False
+
         super().__init__(parent,
                          textvariable=input_string,
                          corner_radius=0,
                          border_width=4,
                          fg_color=ENTRY_BG,
-                         border_color=ENTRY_BG,
+                         border_color=f"#F{color}{color}",
                          text_color=TEXT_COLOR,
                          font=ctk.CTkFont(family=TEXT_FONT,size=TEXT_SIZE))
         self.place(relx=0.5,rely=0.95,anchor="center")
 
         self.bind("<Return>", submit_location)
+
+        input_string.trace("w", self.remove_error)
+
+    def error_animation(self):
+        self.error = True
+        if self.color_index > 0:
+            self.color_index -= 1
+            border_color = f"#F{COLOR_RANGE[self.color_index]}{COLOR_RANGE[self.color_index]}"
+            text_color = f"#{COLOR_RANGE[-self.color_index-1]}00"
+            self.configure(border_color=border_color,text_color=text_color)
+            self.after(40, self.error_animation)
+    
+    def remove_error(self, *args):
+        if self.error:
+            self.configure(border_color=ENTRY_BG,text_color=TEXT_COLOR)
+            self.color_index = 15
 
 App()
